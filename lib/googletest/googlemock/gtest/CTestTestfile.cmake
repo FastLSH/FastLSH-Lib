1ff7a2fbf3aaf91b228b8459064e152d959cbf1a
PeterYAOHAIXU <yaohaixupeter@gmail.com>
2017-04-09 21:15:47 +0800

    clean

--- lib/googletest/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/peter/FYP/FastLSH/lib/googletest/googletest
-# Build directory: /home/peter/FYP/FastLSH/lib/googletest/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
