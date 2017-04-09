092c25ac5ce5548125932d5e55e641ca0dffb210
PeterYAOHAIXU <yaohaixupeter@gmail.com>
2017-04-09 21:10:24 +0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/FastLSH.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/peter/FYP/FastLSH-Lib/src/Computer/OpenMPComputer.cpp" "/home/peter/FYP/FastLSH-Lib/cmake-build-debug/CMakeFiles/FastLSH.dir/src/Computer/OpenMPComputer.cpp.o"
+  "/home/peter/FYP/FastLSH-Lib/src/Computer/PthreadComputer.cpp" "/home/peter/FYP/FastLSH-Lib/cmake-build-debug/CMakeFiles/FastLSH.dir/src/Computer/PthreadComputer.cpp.o"
+  "/home/peter/FYP/FastLSH-Lib/src/Computer/SingleThreadComputer.cpp" "/home/peter/FYP/FastLSH-Lib/cmake-build-debug/CMakeFiles/FastLSH.dir/src/Computer/SingleThreadComputer.cpp.o"
+  "/home/peter/FYP/FastLSH-Lib/src/Computer/StdThreadComputer.cpp" "/home/peter/FYP/FastLSH-Lib/cmake-build-debug/CMakeFiles/FastLSH.dir/src/Computer/StdThreadComputer.cpp.o"
+  "/home/peter/FYP/FastLSH-Lib/src/FileLoader/FileLoader.cpp" "/home/peter/FYP/FastLSH-Lib/cmake-build-debug/CMakeFiles/FastLSH.dir/src/FileLoader/FileLoader.cpp.o"
+  "/home/peter/FYP/FastLSH-Lib/src/FileLoader/HdfsLoader.cpp" "/home/peter/FYP/FastLSH-Lib/cmake-build-debug/CMakeFiles/FastLSH.dir/src/FileLoader/HdfsLoader.cpp.o"
+  "/home/peter/FYP/FastLSH-Lib/src/FileLoader/LfsLoader.cpp" "/home/peter/FYP/FastLSH-Lib/cmake-build-debug/CMakeFiles/FastLSH.dir/src/FileLoader/LfsLoader.cpp.o"
+  "/home/peter/FYP/FastLSH-Lib/src/Generator.cpp" "/home/peter/FYP/FastLSH-Lib/cmake-build-debug/CMakeFiles/FastLSH.dir/src/Generator.cpp.o"
+  "/home/peter/FYP/FastLSH-Lib/src/InMemoryStore/memcached.cpp" "/home/peter/FYP/FastLSH-Lib/cmake-build-debug/CMakeFiles/FastLSH.dir/src/InMemoryStore/memcached.cpp.o"
+  "/home/peter/FYP/FastLSH-Lib/src/InMemoryStore/redis.cpp" "/home/peter/FYP/FastLSH-Lib/cmake-build-debug/CMakeFiles/FastLSH.dir/src/InMemoryStore/redis.cpp.o"
+  "/home/peter/FYP/FastLSH-Lib/src/LSH.cpp" "/home/peter/FYP/FastLSH-Lib/cmake-build-debug/CMakeFiles/FastLSH.dir/src/LSH.cpp.o"
+  "/home/peter/FYP/FastLSH-Lib/src/Normalizer.cpp" "/home/peter/FYP/FastLSH-Lib/cmake-build-debug/CMakeFiles/FastLSH.dir/src/Normalizer.cpp.o"
+  "/home/peter/FYP/FastLSH-Lib/tests/helper.cpp" "/home/peter/FYP/FastLSH-Lib/cmake-build-debug/CMakeFiles/FastLSH.dir/tests/helper.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/hadoop/include"
+  "/usr/lib/jvm/java-8-openjdk-amd64/include"
+  "/usr/lib/jvm/java-8-openjdk-amd64/include/linux"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/FastLSH.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FastLSH.dir/src/LSH.cpp.o"
+  "CMakeFiles/FastLSH.dir/src/InMemoryStore/memcached.cpp.o"
+  "CMakeFiles/FastLSH.dir/src/InMemoryStore/redis.cpp.o"
+  "CMakeFiles/FastLSH.dir/src/Normalizer.cpp.o"
+  "CMakeFiles/FastLSH.dir/src/Generator.cpp.o"
+  "CMakeFiles/FastLSH.dir/src/Computer/PthreadComputer.cpp.o"
+  "CMakeFiles/FastLSH.dir/src/Computer/SingleThreadComputer.cpp.o"
+  "CMakeFiles/FastLSH.dir/src/Computer/OpenMPComputer.cpp.o"
+  "CMakeFiles/FastLSH.dir/src/Computer/StdThreadComputer.cpp.o"
+  "CMakeFiles/FastLSH.dir/src/FileLoader/LfsLoader.cpp.o"
+  "CMakeFiles/FastLSH.dir/src/FileLoader/FileLoader.cpp.o"
+  "CMakeFiles/FastLSH.dir/src/FileLoader/HdfsLoader.cpp.o"
+  "CMakeFiles/FastLSH.dir/tests/helper.cpp.o"
+  "libFastLSH.pdb"
+  "libFastLSH.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FastLSH.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
